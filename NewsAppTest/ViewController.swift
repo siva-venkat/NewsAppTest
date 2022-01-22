a12dd3b7526f64052f4fcf96dddf5fb6fa61f9d3
siva-venkat <85175990+siva-venkat@users.noreply.github.com>
2022-01-22 15:58:47 +1100

    Initial Commit

---
+++ NewsAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppTest
+//
+//  Created by Sivaranjani Venkatesh on 22/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
